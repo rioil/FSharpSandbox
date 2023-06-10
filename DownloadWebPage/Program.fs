4b21fa00100da89cfa551413e416ea3dad58378c
rioil <rioil.dev@gmail.com>
2023-06-10 17:55:07 +0900

    ファイルダウンロードを実装

---
+++ DownloadWebPage/Program.fs
@@ -0,0 +1,23 @@
+﻿open System.Net.Http
+open System.IO
+
+printfn "Download a web page"
+
+let fetchUrl (url : string) =
+    let client = new HttpClient()
+    client.GetStreamAsync(url) |> Async.AwaitTask
+
+let printStreamContent (stream : Stream) =
+    use reader = new StreamReader(stream)
+    while not reader.EndOfStream do
+        printfn "%s" (reader.ReadLine())
+    done
+    printfn
+
+let urls = ["https://rioil.dev";"https://rioil.net"]
+urls
+    |> List.map fetchUrl
+    |> Async.Parallel
+    |> Async.RunSynchronously
+    |> Array.map printStreamContent
+    |> ignore
