a1871b8b3bd12714577f3568de9e4dd74d9c293e
rioil <rioil.dev@gmail.com>
2024-02-27 15:32:58 +0900

    途中まで作成された計算プログラムをコミット

--- SimpleCalculator/Program.fs
+++ SimpleCalculator/Program.fs
@@ -1,8 +1,42 @@
-﻿let squareFunction x = x * x
+﻿open System.Text.RegularExpressions
 
 [<EntryPoint>]
 let main argv =
     printfn "Simple Calculator"
     printfn "%d tokens" argv.Length
-    printfn "%d" (squareFunction 5)
+
+    let digitPattern = new Regex("[+-]?\d+")
+    let operatorPattern = new Regex("[+\-*/]")
+
+    let distinguish x =
+        if digitPattern.IsMatch x then printfn "%s is a digit" x
+        elif operatorPattern.IsMatch x then printfn "%s is an operator" x
+        else printfn "%s is a invalid token" x
+
+    let rec parse list =
+        match list with
+        | [] -> ()
+        | first::rest ->
+            distinguish first
+            parse rest
+
+    let rec calculate x operator y =
+        match operator with
+        | "+" -> x + y
+        | "-" -> x - y
+        | "*" -> x * y
+        | "/" -> x / y
+        | _ -> 0
+
+    //let add2times3 = (+) 2 >> (*) 3
+    //printfn "%d" (add2times3 5)
+
+    //let twoToFive = [2;3;4;5]
+    //let oneToFive = 1 :: twoToFive
+    //let zeroToFive = [0;1] @ twoToFive
+
+    let tokens = ["1";"+";"2";"*";"3"]
+    parse tokens
+    printf "result: %d" (calculate 2 "+" 3)
+
     0
